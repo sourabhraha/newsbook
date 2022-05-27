3309e993ddd217886f163756927c212e8806c071
sourabh <sourabhrahafbg@gmail.com>
2022-05-28 00:16:52 +0530

    updated apis

--- src/config.js
+++ src/config.js
@@ -1,10 +1,6 @@
-
 const apilist = [
-  "73be567473614bfdf5ec112c52088363",
-  "ca95c867f3bf4cede21557475a2f10fb",
-  "44ecd5895721905dae6e0625c69fca96",
-  "7045d5c0aeef7dc46b80c21b36bbbd3c",
-  "4c03eca336175a11538c4b6f30b0d518",
+  "56d19d5063f6f61b08547199ee5c2671",
+  "4c1a9bc9b3fa274cc1facf979d5f790b",
 ];
 let rand_no = Math.floor(Math.random() * apilist.length);
 
